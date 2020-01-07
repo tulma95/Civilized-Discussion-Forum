73acafb4d76654a986e3b28e184effc15838b77f
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-01-07 13:50:29 +0200

    Add route for single thread

--- backend/controllers/threads.js
+++ backend/controllers/threads.js
@@ -6,5 +6,15 @@ threadsRouter.get('/', async (req, res) => {
   res.json(threads)
 })
 
+threadsRouter.get('/:id', async (req, res) => {
+  const thread = await Thread.findAll({
+    where: {
+      id: req.params.id
+    },
+    include: [{ model: Post, as: 'posts' }]
+  })
+
+  res.json(thread)
+})
 
 module.exports = threadsRouter
