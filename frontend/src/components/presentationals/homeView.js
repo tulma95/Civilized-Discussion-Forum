531c89452b8fd2f94e0544c1791a272c4df2d996
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-02-04 15:12:39 +0200

    Revert "Remade HomeView.js"
    This reverts commit 23c39aa2072dec49738325d621cf805b2fd0201a.

--- frontend/src/components/presentationals/homeView.js
+++ frontend/src/components/presentationals/homeView.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const HomeView = () => {
-  return (
-    <div>
-      Welcome to the civilized discussion forum. Begin by choosing category from
-      the left.
-    </div>
-  )
-}
+const homeView = () => (
+  <div>
+    Welcome to the civilized discussion forum. Begin by choosing category from
+    the left.
+  </div>
+)
 
-export default HomeView
+export default homeView
