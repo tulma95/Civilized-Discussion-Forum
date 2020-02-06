953a851bbbc35163183546321f6ecc8ded155c69
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-02-06 18:09:06 +0200

    Add register form

--- frontend/src/components/containers/Login.js
+++ frontend/src/components/containers/Login.js
@@ -2,28 +2,49 @@ import React, { useState } from 'react'
 
 const Login = () => {
   const [username, setUsername] = useState('')
-  const [password, setPassword] = useState()
+  const [password, setPassword] = useState('')
+  const [retypePassword, setRetypePassword] = useState('')
+  const [toggleRegister, setToggleRegister] = useState(false)
 
   const handleSubmit = event => {
     event.preventDefault()
-    console.log('submit')
   }
+
+  const handleChange = setter => event => {
+    setter(event.target.value)
+  }
+
+  const registerInputs = () => (
+    <input
+      value={retypePassword}
+      placeholder='retype password'
+      onChange={handleChange(setRetypePassword)}
+      style={{ width: '100%' }}
+      type='password'
+    />
+  )
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         value={username}
-        onChange={e => setUsername(e.target.value)}
+        placeholder='username'
+        onChange={handleChange(setUsername)}
         style={{ width: '100%' }}
         type='text'
       />
       <input
         value={password}
-        onChange={e => setPassword(e.target.value)}
+        placeholder='password'
+        onChange={handleChange(setPassword)}
         style={{ width: '100%' }}
         type='password'
       />
+      {toggleRegister && registerInputs()}
       <button type='submit'>Login</button>
+      <button onClick={() => setToggleRegister(!toggleRegister)}>
+        {toggleRegister ? 'cancel' : 'register'}
+      </button>
     </form>
   )
 }
