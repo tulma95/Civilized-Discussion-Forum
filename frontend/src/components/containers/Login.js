df25c7c3315630b73c9a429318639b557d66744c
Riku Honkanen <riku.honkanen9595@gmail.com>
2020-02-06 21:36:22 +0200

    Add frontend registeration and validation

--- frontend/src/components/containers/Login.js
+++ frontend/src/components/containers/Login.js
@@ -1,51 +1,83 @@
 import React, { useState } from 'react'
+import logInService from '../../services/userService'
+import './login.scss'
 
 const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [retypePassword, setRetypePassword] = useState('')
+  const username = React.createRef()
+  const password = React.createRef()
+  const retypePassword = React.createRef()
   const [toggleRegister, setToggleRegister] = useState(false)
 
   const handleSubmit = event => {
     event.preventDefault()
+    const user = {
+      username: username.current.value,
+      password: password.current.value
+    }
+    if (toggleRegister) {
+      register(user)
+    } else {
+      login()
+    }
+    username.current.value = ''
+    password.current.value = ''
+    retypePassword.current.value = ''
   }
 
-  const handleChange = setter => event => {
-    setter(event.target.value)
+  const login = () => {}
+
+  const register = user => {
+    logInService.registerUser(user)
+  }
+
+  const validate = () => {
+    if (!retypePassword.current) return
+    if (password.current.value !== retypePassword.current.value) {
+      retypePassword.current.setCustomValidity('Password must match')
+    } else {
+      password.current.setCustomValidity('')
+      retypePassword.current.setCustomValidity('')
+    }
   }
 
   const registerInputs = () => (
     <input
-      value={retypePassword}
+      minLength='5'
+      required
       placeholder='retype password'
-      onChange={handleChange(setRetypePassword)}
-      style={{ width: '100%' }}
+      ref={retypePassword}
       type='password'
+      onChange={validate}
     />
   )
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={username}
-        placeholder='username'
-        onChange={handleChange(setUsername)}
-        style={{ width: '100%' }}
-        type='text'
-      />
-      <input
-        value={password}
-        placeholder='password'
-        onChange={handleChange(setPassword)}
-        style={{ width: '100%' }}
-        type='password'
-      />
-      {toggleRegister && registerInputs()}
-      <button type='submit'>Login</button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          minLength='3'
+          ref={username}
+          placeholder='username'
+          style={{ width: '100%' }}
+          type='text'
+        />
+        <input
+          required
+          minLength='5'
+          ref={password}
+          placeholder='password'
+          style={{ width: '100%' }}
+          type='password'
+          onChange={validate}
+        />
+        {toggleRegister && registerInputs()}
+        <button type='submit'>{toggleRegister ? 'register' : 'login'}</button>
+      </form>
       <button onClick={() => setToggleRegister(!toggleRegister)}>
         {toggleRegister ? 'cancel' : 'register'}
       </button>
-    </form>
+    </div>
   )
 }
 
---
+++ frontend/src/services/userService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const baseUrl = '/api/users'
+
+const registerUser = async newUser => {
+  console.log(newUser)
+}
+
+export default { registerUser }
